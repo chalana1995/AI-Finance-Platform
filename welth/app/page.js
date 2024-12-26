4b688ddb04f58bf24dfa7b9ffb36c48941d19365
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-12-26 20:00:24 +0530

    header part done

--- welth/app/page.js
+++ welth/app/page.js
@@ -2,5 +2,9 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button variant="destructive">Subscribe to RoadSiderCode</Button>
+  return (
+    <div className="mt-40">
+      <Button variant="destructive">Subscribe to RoadSiderCode</Button>
+    </div>
+  );
 }
